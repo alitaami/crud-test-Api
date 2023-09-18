f39a49f23d57ab50d521785c2534b79b27532fbc
Ali Taami <alitaami81@gmail.com>
2023-09-18 21:42:46 +0330

    Generate SeedData codes in Context

--- Insfrastructure/Context/ApplicationDbContext.cs
+++ Insfrastructure/Context/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks; 
+using System.Threading.Tasks;
 
 namespace Insfrastructure.Context
 {
@@ -29,6 +29,31 @@ namespace Insfrastructure.Context
             modelBuilder.Entity<Customer>()
                 .HasIndex(c => c.Email)
                 .IsUnique();
+
+            modelBuilder.Entity<Customer>().HasData(
+            new Customer
+            {
+                Id = 1,
+                Firstname = "Ali",
+                Lastname = "Taami",
+                DateOfBirth = new DateTime(2002, 10, 11),
+                PhoneNumber = "9301327634", // Valid phone number
+                Email = "alitaami81@gmail.com", // Valid email
+                BankAccountNumber = "5859831001081461" // Valid 16-digit bank account number
+            },
+             new Customer
+             {
+                 Id = 2,
+                 Firstname = "Ata",
+                 Lastname = "Taami",
+                 DateOfBirth = new DateTime(2012, 10, 11),
+                 PhoneNumber = "9301327624", // Valid phone number
+                 Email = "atataami91@gmail.com", // Valid email
+                 BankAccountNumber = "5859831001081462" // Valid 16-digit bank account number
+             }
+
+             // Add more customer data as needed
+             );
 
             base.OnModelCreating(modelBuilder);
         }
