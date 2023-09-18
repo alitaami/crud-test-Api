03c63c50ebd35aef2587dd4ab6fca156602da63d
Ali Taami <alitaami81@gmail.com>
2023-09-18 16:43:28 +0330

    Added some features to Insfrastructure

---
+++ Insfrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Insfrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Customer>()
+                .HasIndex(c => new { c.Firstname, c.Lastname, c.DateOfBirth })
+                .IsUnique();
+
+            modelBuilder.Entity<Customer>()
+                .HasIndex(c => c.Email)
+                .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Insfrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Application.Repository;
+using Insfrastructure.Context;
+using Insfrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Insfrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddScoped(typeof(IRepository<>), typeof(Repository<>))
+                           .AddDbContext<ApplicationDbContext>(options => options
+                           .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
