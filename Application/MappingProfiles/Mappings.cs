956e9be68088fa96d8317fc73f812f77c6e7ae6c
Ali Taami <alitaami81@gmail.com>
2023-09-18 17:13:10 +0330

    Added MappingProfiles

---
+++ Application/MappingProfiles/Mappings.cs
@@ -0,0 +1,24 @@
+﻿using Application.Models;
+using Application.Models.Dtos;
+using Application.Models.ViewModels;
+using AutoMapper;
+using Domain;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.MappingProfiles
+{
+    public class Mappings : Profile
+    {
+        public Mappings()
+        {
+            CreateMap<CustomerViewModel, Customer>().ReverseMap();
+            CreateMap<CustomerDto, Customer>().ReverseMap();
+        }
+    }
+}
