0e56295067ca8c48085ad00b1df5c415ad9883a2
Ali Taami <alitaami81@gmail.com>
2023-09-18 17:23:29 +0330

    Added DeleteCustomerRequest  Command

---
+++ Application/Features/Properties/Commands/DeleteCustomerRequest.cs
@@ -0,0 +1,57 @@
+﻿using Application.Features.Behaviors.Contracts;
+using Application.Repository;
+using Application.Services.Interfaces;
+using Common.Resources;
+using Domain;
+using Domain.ApiResult;
+using Domain.Entities;
+using EstateAgentApi.Services.Base;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Properties.Commands
+{
+    public class DeleteCustomerRequest : IRequest<ServiceResult>
+    {
+        public int PropertyId { get; set; }
+
+        public DeleteCustomerRequest(int propertyId)
+        {
+            PropertyId = propertyId;
+        }
+    }
+
+    public class DeletePropertyRequestHandler : ServiceBase<DeletePropertyRequestHandler>, IRequestHandler<DeleteCustomerRequest, ServiceResult>
+    {
+        private readonly ICustomerService _customer;
+        public DeletePropertyRequestHandler(ILogger<DeletePropertyRequestHandler> logger, ICustomerService customer)
+       : base(logger)
+        {
+            _customer = customer;
+        }
+
+        public async Task<ServiceResult> Handle(DeleteCustomerRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var res = await _customer.DeleteCustomerById(request.PropertyId, cancellationToken);
+
+                if (!res.Result.HttpStatusCode.Equals((int)(HttpStatusCode.OK)))
+                    return BadRequest(ErrorCodeEnum.GeneralErrorTryAgain, Resource.GeneralErrorTryAgain, null);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, null, null);
+                return InternalServerError(ErrorCodeEnum.InternalError, Resource.GeneralErrorTryAgain, null);
+            }
+        }
+    }
+}
